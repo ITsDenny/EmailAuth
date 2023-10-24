c30a43d8a8b78e3aa49a6ca7807d4d80f4ce59d1
ITsDenny <dennym.channel@gmail.com>
2023-10-24 11:06:07 +0700

    Memberi relasi many to many

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Classes extends Model
 {
     use HasFactory;
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'class_assignment','class_id','student_id');
+    }
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
     use HasFactory;
+    public function classes()
+    {
+        return $this->belongsToMany(Classes::class, 'classes_assigment','student_id','class_id');   
+    }
 }
